8e6cae2700d9b32b3b5eb30907bf30bb078680a7
Rahul <rkredux@gmail.com>
2017-07-05 19:59:26 -0400

    Removed console.log and added Pets JSON data into the module.

--- src/reducers/reducePets.js
+++ src/reducers/reducePets.js
@@ -1,15 +1,9 @@
-//a reducer takes in two things 
-//the action (info about what happened)
-//return a copy of the new state 
-
-// incomes the store (state) and whatever happened (action)
-// and here comes out the updated store(or state)
 
 
-function reducePets(state=[], action){
-	// console.log("this pet will be edited"); 
-	// console.log(state, action); 
-	
+
+
+function reducePets(state = [], action){
+	console.log(state); 
 	return state; 
 }
 
