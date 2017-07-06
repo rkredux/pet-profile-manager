8ea89af6f9a3e9c34f39ff90d786b74c466256e3
Rahul <rkredux@gmail.com>
2017-07-05 20:01:35 -0400

    Bind React App with Redux store with Provider component and passing the store prop.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.js';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux"; 
 import registerServiceWorker from './registerServiceWorker';
 import './styles/index.css';
 import store from "./Store.js"
+import App from "./components/App.js"
 
 
 ReactDOM.render(
 
-    <Provider store={store}>
+
+<Provider store={store}>
 		<BrowserRouter>
-		 <App></App>
+		 <App/>
 		</BrowserRouter>
-	</Provider>
+</Provider>
 		, document.getElementById('root'));
 
 
