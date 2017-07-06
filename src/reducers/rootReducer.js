d9e8bfe03fc3a71a67b7d7c32d019a8a6e203eca
Rahul <rkredux@gmail.com>
2017-07-05 20:03:51 -0400

    Root reducer not required any more.

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,9 +1,12 @@
 // import { combineReducers } from "redux"; 
 // import { routerReducer } from "react-router-redux";
 // import reducePets from "./reducePets.js"; 
-// // import reduceComments from "./reduceComments.js"; 
+// // // import reduceComments from "./reduceComments.js"; 
 
 // const rootReducer = combineReducers({reducePets, routing: routerReducer}); 
 
 // export default rootReducer; 
+
+
+
 
