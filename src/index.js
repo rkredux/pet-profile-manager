59bd475e58bb7bf6f64b017173db1615627ae061
Rahul <rkredux@gmail.com>
2017-07-12 06:31:25 -0400

    Wrap App component in Material UI Component from Google Material UI Library.

--- src/index.js
+++ src/index.js
@@ -4,19 +4,30 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux"; 
 import registerServiceWorker from './registerServiceWorker';
 import './styles/index.css';
-import store from "./Store.js"
-import App from "./components/App.js"
+import store from "./Store.js"; 
+import App from "./components/App.js";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; 
+import injectTapEventPlugin from 'react-tap-event-plugin'; 
+
+
+const WrappedApp = () => (
+	<MuiThemeProvider>
+		<App></App>
+	</MuiThemeProvider>
+	); 
+
 
 
 ReactDOM.render(
 
+	<Provider store={store}>
+			<BrowserRouter>
+			 <WrappedApp/>
+			</BrowserRouter>
+	</Provider>
+	,document.getElementById('root')
 
-<Provider store={store}>
-		<BrowserRouter>
-		 <App/>
-		</BrowserRouter>
-</Provider>
-		, document.getElementById('root'));
+);
 
 
 registerServiceWorker();
